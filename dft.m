6ac783de92acc8f70f4e354c2a5d7ad73bfb8950
Jesper Josefsson <jesper.josefsson@gmail.com>
2011-10-05 17:06:42 +0200

    dftarbete

--- dft.m
+++ dft.m
@@ -1,13 +1,17 @@
+series = [1.2732 0.4244 0.2546];
+estimated = [1.2702 0.4154 0.2398];
+
+
+
 N = 2^13;
 fs = 100;
 Ts = 1/100;
-t = 0:Ts:10-Ts;
+t = 0:Ts:(N-1)*Ts;
 x = square(t);
 % plot(t,x);
 
 k = 0:(N-1);
 wk = 2*pi*fs*k/N;
-
 X = fft(x,N);
 
-plot(wk, X)
+plot(wk, abs(X))
