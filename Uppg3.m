97ee10bd89ee30fc40d7ca4d4b0e2bfef438b89d
Jesper Josefsson <jesper.josefsson@gmail.com>
2011-10-05 15:42:58 +0200

    fixade med Uppg3 för tydligare uträkningssteg

--- Uppg3.m
+++ Uppg3.m
@@ -3,8 +3,14 @@ w=2*pi/T;
 M=200;
 t=T*(0:M-1)/M;
 y = @(t) 0;
-for k=1:50
-  b = mod(k,2)*(4/(k*pi));
-  y = @(t) y(t) + b*sin(k*w*t);
+bs = [];
+for k=1:100
+  ck = -(mod(k, 2))*((1i*2)/(pi*k));
+  cminusk = -(mod(k, 2))*((1i*2)/(pi*(-k)));
+  ak = ck + cminusk;
+  bk = 2i*ck;
+  bs = [bs bk];
+  y = @(t) y(t) + ak*cos(k*w*t) + bk*sin(k*w*t);
 end
+bs
 plot(t, y(t))
